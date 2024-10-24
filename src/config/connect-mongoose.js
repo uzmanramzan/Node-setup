c90c8cbef5eb1721eb94d317acd1895b0e9c1939
Code Warriors <114612630+codeWarriors-pro@users.noreply.github.com>
2024-10-24 06:46:35 -0700

    added mongo

--- src/config/connect-mongoose.js
+++ src/config/connect-mongoose.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 (async () => {
   try {
-    const result = await mongoose.connect(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    console.log("db connected===");
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log("db connected");
   } catch (error) {
     console.log(error.message);
